f47048e88f6095aa2495d111f4fe26bef86af370
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-02-23 18:47:07 +0100

    add missing dummy functions if smoltcp isn't enabled

--- hermit-sys/src/dummy.rs
+++ hermit-sys/src/dummy.rs
@@ -5,21 +5,66 @@
 pub struct Handle(usize);
 
 #[no_mangle]
-pub fn sys_socket_connect(_ip: &[u8], _port: u16, _timeout: Option<u64>) -> Result<Handle, ()> {
+pub fn sys_tcp_stream_connect(_ip: &[u8], _port: u16, _timeout: Option<u64>) -> Result<Handle, ()> {
 	Err(())
 }
 
 #[no_mangle]
-pub fn sys_socket_read(_handle: Handle, _buffer: &mut [u8]) -> Result<usize, ()> {
+pub fn sys_tcp_stream_read(_handle: Handle, _buffer: &mut [u8]) -> Result<usize, ()> {
 	Err(())
 }
 
 #[no_mangle]
-pub fn sys_socket_write(_handle: Handle, _buffer: &[u8]) -> Result<usize, ()> {
+pub fn sys_tcp_stream_write(_handle: Handle, _buffer: &[u8]) -> Result<usize, ()> {
 	Err(())
 }
 
 #[no_mangle]
-pub fn sys_socket_close(_handle: Handle) -> Result<(), ()> {
+pub fn sys_tcp_stream_close(_handle: Handle) -> Result<(), ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_set_read_timeout(_handle: Handle, _timeout: Option<u64>) -> Result<(), ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_get_read_timeout(_handle: Handle) -> Result<Option<u64>, ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_set_write_timeout(_handle: Handle, _timeout: Option<u64>) -> Result<(), ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_get_write_timeout(_handle: Handle) -> Result<Option<u64>, ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_duplicate(_handle: Handle) -> Result<Handle, ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_peek(_handle: Handle, _buf: &mut [u8]) -> Result<usize, ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_set_nonblocking(_handle: Handle, _mode: bool) -> Result<(), ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_set_tll(_handle: Handle, _ttl: u32) -> Result<(), ()> {
+	Err(())
+}
+
+#[no_mangle]
+pub fn sys_tcp_stream_get_tll(_handle: Handle) -> Result<u32, ()> {
 	Err(())
 }
