113298124548729ef0487e8aa8f73ffd60ca7a1a
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-04-11 11:57:10 +0200

    extend demo by a parallel matrix multiplication

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -66,7 +66,7 @@ fn main() {
 		stringify!(pi_parallel),
 		test_result(pi_parallel(2, 5000000))
 	);
-	/*println!(
+	println!(
 		"Test {} ... {}",
 		stringify!(laplace),
 		test_result(laplace(128, 128))
@@ -75,7 +75,7 @@ fn main() {
 		"Test {} ... {}",
 		stringify!(test_matmul_strassen),
 		test_result(test_matmul_strassen())
-	);*/
+	);
 	/*println!(
 		"Test {} ... {}",
 		stringify!(thread_creation),
