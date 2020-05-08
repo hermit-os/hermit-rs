f3c3605ab8a9a122c13f700e15bbafabb597822b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-05-09 01:01:55 +0200

    simplify IP benchmark

--- netbench/src/rust-tcp-bw/client.rs
+++ netbench/src/rust-tcp-bw/client.rs
@@ -20,21 +20,17 @@ fn main() {
 		println!("Connection established! Ready to send...");
 
 		// Create a buffer of 0s, size n_bytes, to be sent over multiple times
-		let mut buf = vec![0; n_bytes];
-		let progress_tracking_percentage = n_rounds / 100;
+		let buf = vec![0; n_bytes];
 
-		for i in 0..n_rounds {
+		for _i in 0..n_rounds {
 			match stream.write_all(&buf) {
 				Ok(_n) => {}
 				Err(err) => panic!("crazy stuff happened while sending {}", err),
 			}
-			if i % progress_tracking_percentage == 0 {
-				println!("{}% completed", i / progress_tracking_percentage);
-			}
 		}
-		println!("Sent everything!");
-
 		connection::close_connection(&stream);
+
+		println!("Sent everything!");
 	} else {
 		println!("Couldn't connect to server...");
 	}
