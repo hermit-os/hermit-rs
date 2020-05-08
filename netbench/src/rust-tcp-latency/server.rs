2bee7b38788205f538c97adf761b04d9df0e1d18
Stefan Lankes <stlankes@users.noreply.github.com>
2020-05-08 08:23:21 +0200

    add simple benchmark to evaluate the bandwidth / latency (#16)
    * add simple benchmark to evaluate the performance
    
    * The benchmark is derived from on rust-tcp-io-perf. A detail description is published in the readme.
    
    * all network functions are able to return errors because the network initialization is able to fail
    
    * test also the branch devel

---
+++ netbench/src/rust-tcp-latency/server.rs
@@ -0,0 +1,29 @@
+#![allow(unused_imports)]
+
+extern crate bytes;
+#[cfg(target_os = "hermit")]
+extern crate hermit_sys;
+extern crate rust_tcp_io_perf;
+
+use rust_tcp_io_perf::config;
+use rust_tcp_io_perf::connection;
+use rust_tcp_io_perf::threading;
+
+fn main() {
+	let args = config::parse_config();
+	let n_bytes = args.n_bytes;
+	let n_rounds = args.n_rounds;
+	let mut buf = vec![0; n_bytes];
+
+	let mut stream = connection::server_listen_and_get_first_connection(&args.port);
+	connection::setup(&args, &mut stream);
+	threading::setup(&args);
+
+	// Make sure n_rounds is the same between client and server
+	for _i in 0..(n_rounds * 2) {
+		connection::receive_message(n_bytes, &mut stream, &mut buf);
+		connection::send_message(n_bytes, &mut stream, &buf);
+	}
+
+	println!("Done exchanging stuff")
+}
