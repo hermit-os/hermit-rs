b77499ee924ccc66eccda5b9f2038c65c1f86863
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-05-21 16:07:50 +0200

    remove obsolete line

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -28,7 +28,6 @@ fn test_result<T>(result: Result<(), T>) -> &'static str {
 }
 
 fn main() {
-	println!("HH");
 	println!("Test {} ... {}", stringify!(hello), test_result(hello()));
 	println!(
 		"Test {} ... {}",
