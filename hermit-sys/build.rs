d4bbfa3dcbfc59188addadda98e3faa73dd4ce14
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2020-05-21 14:53:01 +0200

    combine master and devel branch together
    To simplify the development, the feature "with_submodule" is introduced.
    If this is set, a submodule with the kernel is expected and use this
    submodule to build the library operating system. Otherwise,
    it use the source code from crates.io.
    Per default, the code from crates.io is used. However, the examples of this
    repository use the kernel as submodule.

--- hermit-sys/build.rs
+++ hermit-sys/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process::Command;
 
-#[cfg(not(feature = "rustc-dep-of-std"))]
+#[cfg(all(not(feature = "rustc-dep-of-std"), not(feature = "with_submodule")))]
 fn build() {
 	#[cfg(windows)]
 	let out_dir = env::temp_dir().to_str().unwrap().to_owned();
@@ -52,6 +52,53 @@ fn build() {
 
 	println!(
 		"cargo:rustc-link-search=native={}/rusty-hermit/target/x86_64-unknown-hermit-kernel/{}",
+		out_dir.clone(),
+		profile
+	);
+	println!("cargo:rustc-link-lib=static=hermit");
+}
+
+#[cfg(all(not(feature = "rustc-dep-of-std"), feature = "with_submodule"))]
+fn build() {
+	let out_dir = env::var("OUT_DIR").unwrap();
+	let target_dir = out_dir.clone() + "/target";
+	let profile = env::var("PROFILE").expect("PROFILE was not set");
+
+	let mut cmd = Command::new("cargo");
+	cmd.current_dir("../libhermit-rs")
+		.arg("build")
+		.arg("-Z")
+		.arg("build-std=core,alloc")
+		.arg("--target")
+		.arg("x86_64-unknown-hermit-kernel")
+		.arg("--target-dir")
+		.arg(target_dir);
+
+	if profile == "release" {
+		cmd.arg("--release");
+	}
+
+	#[cfg(feature = "instrument")]
+	cmd.env("RUSTFLAGS", "-Z instrument-mcount");
+	// if instrument is not set, ensure that instrument is not in environment variables!
+	#[cfg(not(feature = "instrument"))]
+	cmd.env(
+		"RUSTFLAGS",
+		env::var("RUSTFLAGS")
+			.unwrap_or("".into())
+			.replace("-Z instrument-mcount", ""),
+	);
+
+	let output = cmd.output().expect("Unable to build kernel");
+	let stdout = std::string::String::from_utf8(output.stdout);
+	let stderr = std::string::String::from_utf8(output.stderr);
+	println!("Build libhermit-rs output-status: {}", output.status);
+	println!("Build libhermit-rs output-stdout: {}", stdout.unwrap());
+	println!("Build libhermit-rs output-stderr: {}", stderr.unwrap());
+	assert!(output.status.success());
+
+	println!(
+		"cargo:rustc-link-search=native={}/target/x86_64-unknown-hermit-kernel/{}",
 		out_dir.clone(),
 		profile
 	);
