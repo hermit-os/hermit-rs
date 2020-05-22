6729743cf5769bede6a5d034f0415d6716ec0adb
Stefan Lankes <stlankes@users.noreply.github.com>
2020-05-22 23:35:08 +0200

    combine master and devel branch together (#28)
    To simplify the development, the feature "with_submodule" is introduced. If this is set, a submodule with the kernel is expected and use this submodule to build the library operating system. Otherwise, it use the source code from crates.io. Per default, the code from crates.io is used. However, the examples of this repository use the kernel as submodule.

--- hermit-sys/build.rs
+++ hermit-sys/build.rs
@@ -1,7 +1,10 @@
+extern crate walkdir;
+
 use std::env;
 use std::process::Command;
+use walkdir::{DirEntry, WalkDir};
 
-#[cfg(not(feature = "rustc-dep-of-std"))]
+#[cfg(all(not(feature = "rustc-dep-of-std"), not(feature = "with_submodule")))]
 fn build() {
 	#[cfg(windows)]
 	let out_dir = env::temp_dir().to_str().unwrap().to_owned();
@@ -58,7 +61,79 @@ fn build() {
 	println!("cargo:rustc-link-lib=static=hermit");
 }
 
+#[cfg(all(not(feature = "rustc-dep-of-std"), feature = "with_submodule"))]
+fn build() {
+	let out_dir = env::var("OUT_DIR").unwrap();
+	let target_dir = out_dir.clone() + "/target";
+	let profile = env::var("PROFILE").expect("PROFILE was not set");
+
+	let mut cmd = Command::new("cargo");
+	cmd.current_dir("../libhermit-rs")
+		.arg("build")
+		.arg("-Z")
+		.arg("build-std=core,alloc")
+		.arg("--target")
+		.arg("x86_64-unknown-hermit-kernel")
+		.arg("--target-dir")
+		.arg(target_dir);
+
+	if profile == "release" {
+		cmd.arg("--release");
+	}
+
+	#[cfg(feature = "instrument")]
+	cmd.env("RUSTFLAGS", "-Z instrument-mcount");
+	// if instrument is not set, ensure that instrument is not in environment variables!
+	#[cfg(not(feature = "instrument"))]
+	cmd.env(
+		"RUSTFLAGS",
+		env::var("RUSTFLAGS")
+			.unwrap_or("".into())
+			.replace("-Z instrument-mcount", ""),
+	);
+
+	let output = cmd.output().expect("Unable to build kernel");
+	let stdout = std::string::String::from_utf8(output.stdout);
+	let stderr = std::string::String::from_utf8(output.stderr);
+	println!("Build libhermit-rs output-status: {}", output.status);
+	println!("Build libhermit-rs output-stdout: {}", stdout.unwrap());
+	println!("Build libhermit-rs output-stderr: {}", stderr.unwrap());
+	assert!(output.status.success());
+
+	println!(
+		"cargo:rustc-link-search=native={}/target/x86_64-unknown-hermit-kernel/{}",
+		out_dir.clone(),
+		profile
+	);
+	println!("cargo:rustc-link-lib=static=hermit");
+}
+
+#[cfg(all(not(feature = "rustc-dep-of-std"), feature = "with_submodule"))]
+fn configure_cargo_rerun_if_changed() {
+	fn is_not_ignored(entry: &DirEntry) -> bool {
+		// Ignore .git .vscode and target directories, but not .cargo or .github
+		if entry.depth() == 1 && entry.path().is_dir() {
+			if entry.path().ends_with("target")
+				|| entry.path().ends_with(".git")
+				|| entry.path().ends_with(".vscode")
+			{
+				return false;
+			}
+		}
+		true
+	}
+
+	WalkDir::new("../libhermit-rs")
+		.into_iter()
+		.filter_entry(|e| is_not_ignored(e))
+		.filter_map(|v| v.ok())
+		.filter_map(|v| v.path().canonicalize().ok())
+		.for_each(|x| println!("cargo:rerun-if-changed={}", x.display()));
+}
+
 fn main() {
+	#[cfg(all(not(feature = "rustc-dep-of-std"), feature = "with_submodule"))]
+	configure_cargo_rerun_if_changed();
 	#[cfg(not(feature = "rustc-dep-of-std"))]
 	build();
 }
