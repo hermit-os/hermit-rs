6c33bcbdde866a9b5c588d24d615e673b4ae322f
Joe Richey <joerichey@google.com>
2021-01-14 01:27:54 -0800

    Allow hermit-abi to build without std
    This makes crates depending on hermit-abi able to check their build by
    just using `-Zbuild-std=core` instead of needing all of `-Zbuild-std`.
    
    Also, remove an unnecessary feature that isn't used anywhere.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- hermit-abi/src/lib.rs
+++ hermit-abi/src/lib.rs
@@ -1,8 +1,7 @@
 //! `hermit-abi` is small interface to call functions from the unikernel
 //! [RustyHermit](https://github.com/hermitcore/libhermit-rs).
 
-#![cfg_attr(feature = "rustc-dep-of-std", no_std)]
-#![feature(const_raw_ptr_to_usize_cast)]
+#![no_std]
 extern crate libc;
 
 pub mod tcplistener;
