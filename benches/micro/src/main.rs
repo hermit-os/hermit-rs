a89897bddc2da915207ec0651473d9ab81dfc689
Martin Kröning <mkroening@posteo.net>
2021-10-29 17:27:28 +0200

    Remove license headers

--- benches/micro/src/main.rs
+++ benches/micro/src/main.rs
@@ -1,10 +1,3 @@
-// Copyright (c) 2019 Stefan Lankes, RWTH Aachen University
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![feature(test)]
