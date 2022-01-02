hermit-os/hermit-rs

commit 689deb21142a58706e5fbfc14ac98eff36bf16d1

add option to enable fsgsbase support

hermit-sys/build.rs
@@ -46,6 +46,13 @@ fn build_hermit(src_dir: &Path, target_dir_opt: Option<&Path>) {
 		cmd.arg("--release");
 	}
 
+	cmd.arg("--no-default-features");
+	cmd.arg("--features");
+	#[cfg(feature = "fsgs_base")]
+	cmd.arg("pci,acpi,fsgs_base");
+	#[cfg(not(feature = "fsgs_base"))]
+	cmd.arg("pci,acpi");
+
 	#[cfg(feature = "instrument")]
 	cmd.env("RUSTFLAGS", "-Z instrument-mcount");
 	// if instrument is not set, ensure that instrument is not in environment variables!

hermit-sys/Cargo.toml
@@ -17,6 +17,7 @@ documentation = "https://hermitcore.github.io/rusty-hermit/hermit_sys"
 # controlls whether RustyHermit is build with -Z instrument-mcount
 instrument = ["rftrace"]
 trace = []
+fsgsbase = []
 # only for internal usage, please don't use it
 with_submodule = []
 

commit 9c2fc79cacacd646a950093534cbf194d764fafe

build.rs: Fix fsgsbase feature forwarding
689deb21142a58706e5fbfc14ac98eff36bf16d1 introduced this typo.

hermit-sys/build.rs
@@ -83,10 +83,10 @@ fn build_hermit(src_dir: &Path, target_dir_opt: Option<&Path>) {
 	}
 
 	// do we have to enable FSGSBASE support?
-	#[cfg(feature = "fsgs_base")]
+	#[cfg(feature = "fsgsbase")]
 	{
 		cmd.arg("--features");
-		cmd.arg("fsgs_base");
+		cmd.arg("fsgsbase");
 	}
 
 	// do we support multi-processor systems?