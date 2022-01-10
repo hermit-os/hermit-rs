88ea2f7776e770237197a00a172e3392b8b3ddf8
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2022-01-10 12:07:51 +0100

    address clippy error

--- hermit-sys/src/net/device.rs
+++ hermit-sys/src/net/device.rs
@@ -121,17 +121,17 @@ impl NetworkInterface<HermitNet> {
 			}
 		};
 		let myip: Ipv4Addr = std::env::var("HERMIT_IP")
-			.unwrap_or(HERMIT_IP.to_string())
+			.unwrap_or_else(|_| HERMIT_IP.to_string())
 			.parse()
 			.expect("Unable to parse IPv4 address");
 		let myip = myip.octets();
 		let mygw: Ipv4Addr = std::env::var("HERMIT_GATEWAY")
-			.unwrap_or(HERMIT_GATEWAY.to_string())
+			.unwrap_or_else(|_| HERMIT_GATEWAY.to_string())
 			.parse()
 			.expect("Unable to parse IPv4 address");
 		let mygw = mygw.octets();
 		let mymask: Ipv4Addr = std::env::var("HERMIT_MASK")
-			.unwrap_or(HERMIT_MASK.to_string())
+			.unwrap_or_else(|_| HERMIT_MASK.to_string())
 			.parse()
 			.expect("Unable to parse IPv4 address");
 		let mymask = mymask.octets();
