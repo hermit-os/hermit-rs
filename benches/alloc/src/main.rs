cd0ff3bca344f8101551bca850de1355e384cd2d
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-07-22 22:15:37 +0200

    add copyright notice

--- benches/alloc/src/main.rs
+++ benches/alloc/src/main.rs
@@ -1,6 +1,32 @@
 /*
- * This benchmark is derived from Shaun Beautement's benchmark,
- * which is part of the crate `talc`.
+MIT License
+
+Copyright (c) 2022 Philipp Schuster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* Heavily modified by Shaun Beautement. All errors are probably my own. */
+
+/*
+ * This benchmark was revised for RustyHermit by Stefan Lankes.
+ * The original code is part of the crate `talc`.
  */
 
 #![feature(slice_ptr_get)]
