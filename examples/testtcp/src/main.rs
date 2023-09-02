71f85cfcd04a733f380968f429a35d51fbf749f5
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-09-02 11:06:12 +0200

    add simple program to test the TCP interface

---
+++ examples/testtcp/src/main.rs
@@ -0,0 +1,32 @@
+#[cfg(target_os = "hermit")]
+use hermit as _;
+
+use std::io::Read;
+use std::net::TcpListener;
+
+// demo program to test the tcp interface
+//
+// The program is used to demonstrate issue hermit-os/kernel#880
+// Use `socat - TCP:localhost:8080` to communicate with the
+// unikernel.
+
+fn main() {
+	let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+	let (mut socket, _) = listener.accept().unwrap();
+	let mut buf = [0u8; 1000];
+	loop {
+		println!("about to read");
+		match socket.read(&mut buf) {
+			Err(e) => {
+				println!("read err {e:?}");
+				break;
+			}
+			Ok(received) => {
+				print!("read {}", std::str::from_utf8(&buf[..received]).unwrap());
+				if n == 0 {
+					break;
+				}
+			}
+		}
+	}
+}
