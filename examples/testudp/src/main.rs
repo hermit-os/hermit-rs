b247e73fc388622a0970ed42ff5047d77096ec0c
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-09-03 11:03:08 +0200

    leave test if the program received a message `exit`

--- examples/testudp/src/main.rs
+++ examples/testudp/src/main.rs
@@ -17,10 +17,13 @@ fn main() {
 		// If `buf` is too small to hold, the message, it will be cut off.
 		println!("about to recv");
 		match socket.recv(&mut buf) {
-			Ok(received) => print!(
-				"received {}",
-				std::str::from_utf8(&buf[..received]).unwrap()
-			),
+			Ok(received) => {
+				let msg = std::str::from_utf8(&buf[..received]).unwrap();
+				print!("received {}", msg);
+				if msg.starts_with("exit") {
+					break;
+				}
+			}
 			Err(e) => {
 				println!("recv function failed: {e:?}");
 				break;
