74d1512b4ce54b6f643500c5edb30d5607d4911b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2024-07-14 23:48:38 +0200

    extend support of WASI preview 1

--- examples/wasmtime/src/preview1.rs
+++ examples/wasmtime/src/preview1.rs
@@ -1,11 +1,51 @@
-use std::sync::OnceLock;
+use std::sync::{Mutex, OnceLock};
 use std::time::{Instant, SystemTime};
 
 use anyhow::Result;
-use log::info;
+use bitflags::bitflags;
+use log::debug;
 use wasi::*;
 use wasmtime::{AsContext, AsContextMut, Caller, Extern};
 use zerocopy::AsBytes;
+
+static FD: Mutex<Vec<Descriptor>> = Mutex::new(Vec::new());
+
+#[derive(Debug, Clone, PartialEq)]
+struct FileStream {
+	pub raw_fd: i32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Descriptor {
+	None,
+	Stdin,
+	Stdout,
+	Stderr,
+	Directory(String),
+	File(FileStream),
+}
+
+impl Descriptor {
+	#[inline]
+	pub fn is_none(&self) -> bool {
+		*self == Self::None
+	}
+}
+
+bitflags! {
+	   /// Options for opening files
+	   #[derive(Debug, Copy, Clone, Default)]
+	   pub(crate) struct Oflags: i32 {
+			   /// Create file if it does not exist.
+			   const OFLAGS_CREAT = 1 << 0;
+			   /// Fail if not a directory.
+			   const OFLAGS_DIRECTORY = 1 << 1;
+			   /// Fail if file already exists.
+			   const OFLAGS_EXCL = 1 << 2;
+			   /// Truncate file to size 0.
+			   const OFLAGS_TRUNC = 1 << 3;
+	   }
+}
 
 fn cvt(err: i32) -> i32 {
 	match err {
@@ -17,7 +57,15 @@ fn cvt(err: i32) -> i32 {
 }
 
 pub(crate) fn init<T>(linker: &mut wasmtime::Linker<T>) -> Result<()> {
-	info!("Initialize module wasi_snapshot_preview1");
+	debug!("Initialize module wasi_snapshot_preview1");
+
+	{
+		let mut guard = FD.lock().unwrap();
+		guard.push(Descriptor::Stdin);
+		guard.push(Descriptor::Stdout);
+		guard.push(Descriptor::Stderr);
+		guard.push(Descriptor::Directory(String::from("tmp")));
+	}
 
 	// In case WASI, it is required to emulate
 	// https://github.com/WebAssembly/WASI/blob/main/legacy/preview1/docs.md
@@ -71,12 +119,199 @@ pub(crate) fn init<T>(linker: &mut wasmtime::Linker<T>) -> Result<()> {
 	linker
 		.func_wrap(
 			"wasi_snapshot_preview1",
+			"path_open",
+			|mut caller: Caller<'_, _>,
+			 _fd: i32,
+			 _dirflags: i32,
+			 path_ptr: i32,
+			 path_len: i32,
+			 oflags: i32,
+			 _fs_rights_base: Rights,
+			 _fs_rights_inheriting: Rights,
+			 _fdflags: i32,
+			 fd_ptr: i32| {
+				let oflags = Oflags::from_bits(oflags).unwrap();
+				if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
+					let mut path = vec![0u8; path_len.try_into().unwrap()];
+
+					let _ = mem.read(
+						caller.as_context_mut(),
+						path_ptr.try_into().unwrap(),
+						path.as_bytes_mut(),
+					);
+					let path = "/".to_owned() + std::str::from_utf8(&path).unwrap();
+
+					let mut flags: i32 = 0;
+					if oflags.contains(Oflags::OFLAGS_CREAT) {
+						flags |= hermit_abi::O_CREAT;
+					}
+					/*if oflags.contains(Oflags::OFLAGS_TRUNC) {
+						flags |= hermit_abi::O_TRUNC;
+					}*/
+					flags |= hermit_abi::O_RDWR;
+
+					let mut c_path = vec![0u8; path.len() + 1];
+					c_path[..path.len()].copy_from_slice(path.as_bytes());
+					{
+						let raw_fd = unsafe { hermit_abi::open(c_path.as_ptr() as *const i8, flags, 0)};
+						let mut guard = FD.lock().unwrap();
+						for (i, entry) in guard.iter_mut().enumerate() {
+							if entry.is_none() {
+								*entry = Descriptor::File(FileStream { raw_fd });
+								let _ = mem.write(
+									caller.as_context_mut(),
+									fd_ptr.try_into().unwrap(),
+									i.as_bytes(),
+								);
+
+								return ERRNO_SUCCESS.raw() as i32;
+							}
+						}
+						guard.push(Descriptor::File(FileStream { raw_fd }));
+
+						let new_fd: i32 = (guard.len() - 1).try_into().unwrap();
+						let _ = mem.write(
+							caller.as_context_mut(),
+							fd_ptr.try_into().unwrap(),
+							new_fd.as_bytes(),
+						);
+					}
+
+					return ERRNO_SUCCESS.raw() as i32;
+				}
+
+				ERRNO_INVAL.raw() as i32
+			},
+		)
+		.unwrap();
+	linker
+		.func_wrap(
+			"wasi_snapshot_preview1",
+			"path_unlink_file",
+			|mut caller: Caller<'_, _>, _fd: i32, path_ptr: i32, path_len: i32| {
+				if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
+					let mut path = vec![0u8; path_len.try_into().unwrap()];
+
+					let _ = mem.read(
+						caller.as_context_mut(),
+						path_ptr.try_into().unwrap(),
+						path.as_bytes_mut(),
+					);
+
+					let path = "/".to_owned() + std::str::from_utf8(&path).unwrap();
+					std::fs::remove_file(path).unwrap();
+				}
+
+				ERRNO_SUCCESS.raw() as i32
+			},
+		)
+		.unwrap();
+	linker
+		.func_wrap(
+			"wasi_snapshot_preview1",
+			"fd_prestat_get",
+			|mut caller: Caller<'_, _>, fd: i32, prestat_ptr: i32| {
+				let guard = FD.lock().unwrap();
+				if fd < guard.len().try_into().unwrap() {
+					if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
+						if let Descriptor::Directory(name) = &guard[fd as usize] {
+							let stat = Prestat {
+								tag: PREOPENTYPE_DIR.raw(),
+								u: PrestatU {
+									dir: PrestatDir { pr_name_len: name.len() },
+								},
+							};
+
+							let _ = mem.write(
+								caller.as_context_mut(),
+								prestat_ptr.try_into().unwrap(),
+								unsafe {
+									std::slice::from_raw_parts(
+										(&stat as *const _) as *const u8,
+										size_of::<Prestat>(),
+									)
+								},
+							);
+
+							return ERRNO_SUCCESS.raw() as i32
+						}
+					}
+				}
+				
+				ERRNO_BADF.raw() as i32
+			},
+		)
+		.unwrap();
+	linker
+		.func_wrap(
+			"wasi_snapshot_preview1",
+			"fd_prestat_dir_name",
+			|mut caller: Caller<'_, _>, fd: i32, path_ptr: i32, path_len: i32| {
+				let guard = FD.lock().unwrap();
+				if fd < guard.len().try_into().unwrap() {
+					if let Descriptor::Directory(path) = &guard[fd as usize] {
+						if let Some(Extern::Memory(mem)) = caller.get_export(
+							"memory
+",
+						) {
+							if path_len < path.len().try_into().unwrap() {
+								return ERRNO_INVAL.raw() as i32;
+							}
+			
+							let _ = mem.write(
+								caller.as_context_mut(),
+								path_ptr.try_into().unwrap(),
+								path.as_bytes(),
+							);
+						}
+
+						return ERRNO_SUCCESS.raw() as i32;
+					}
+				}
+				
+				ERRNO_BADF.raw() as i32
+			},
+		)
+		.unwrap();
+	linker
+		.func_wrap("wasi_snapshot_preview1", "fd_close", |fd: i32| {
+			let mut guard = FD.lock().unwrap();
+			if fd < guard.len().try_into().unwrap() {
+				if let Descriptor::File(file) = &guard[fd as usize] {
+					unsafe {
+						hermit_abi::close(file.raw_fd);
+					}
+					guard[fd as usize] = Descriptor::None;
+				}
+			}
+
+			ERRNO_SUCCESS.raw() as i32
+		})
+		.unwrap();
+	linker
+		.func_wrap(
+			"wasi_snapshot_preview1",
 			"fd_write",
 			|mut caller: Caller<'_, _>,
 			 fd: i32,
 			 iovs_ptr: i32,
 			 iovs_len: i32,
 			 nwritten_ptr: i32| {
+				let fd = if fd <= 2 {
+					fd
+				} else {
+					let guard = FD.lock().unwrap();
+					if fd >= guard.len().try_into().unwrap() {
+						return ERRNO_INVAL.raw() as i32;
+					}
+
+					if let Descriptor::File(file) = &guard[fd as usize] {
+						file.raw_fd
+					} else {
+						return ERRNO_INVAL.raw() as i32;
+					}
+				};
+		
 				if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
 					let mut iovs = vec![0i32; (2 * iovs_len).try_into().unwrap()];
 					let _ = mem.read(
