996b247e93c107e8ac292281e1fb86c0c38bd67e
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-09-16 13:52:30 +0200

    refactor(wasmtime/build.rs): extract package
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- examples/wasmtime/build.rs
+++ examples/wasmtime/build.rs
@@ -5,25 +5,18 @@ fn main() -> io::Result<()> {
 	let cargo = env::var_os("CARGO").unwrap();
 	let out_dir = env::var_os("OUT_DIR").unwrap();
 
-	#[cfg(not(feature = "ci"))]
+	let package = if cfg!(feature = "ci") {
+		"hello_world"
+	} else {
+		"wasm-test"
+	};
+
 	let status = Command::new(cargo)
 		.arg("build")
 		.arg("-Zunstable-options")
 		.arg("-Zbuild-std=std,panic_abort")
 		.arg("--target=wasm32-wasi")
-		.arg("--package=wasm-test")
-		.arg("--release")
-		.arg("--target-dir=target")
-		.arg("--artifact-dir")
-		.arg(&out_dir)
-		.status()?;
-	#[cfg(feature = "ci")]
-	let status = Command::new(cargo)
-		.arg("build")
-		.arg("-Zunstable-options")
-		.arg("-Zbuild-std=std,panic_abort")
-		.arg("--target=wasm32-wasi")
-		.arg("--package=hello_world")
+		.args(["--package", package])
 		.arg("--release")
 		.arg("--target-dir=target")
 		.arg("--artifact-dir")
