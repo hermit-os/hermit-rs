385d20686cc4e8daf13fcb668532c59b8d2fa29e
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-09-16 16:48:19 +0200

    fix(wasmtime/build.rs): unpin wasm toolchain
    This reverts commit 3a82ed6ee52be3fefadc94be29f4a6add876ea7d.

--- examples/wasmtime/build.rs
+++ examples/wasmtime/build.rs
@@ -1,9 +1,8 @@
-use std::path::PathBuf;
 use std::process::Command;
 use std::{env, io};
 
 fn main() -> io::Result<()> {
-	let mut cargo = cargo();
+	let cargo = env::var_os("CARGO").unwrap();
 	let out_dir = env::var_os("OUT_DIR").unwrap();
 
 	let package = if cfg!(feature = "ci") {
@@ -12,8 +11,7 @@ fn main() -> io::Result<()> {
 		"wasm-test"
 	};
 
-	cargo
-		.arg("+nightly-2024-07-31")
+	let status = Command::new(cargo)
 		.arg("build")
 		.arg("-Zunstable-options")
 		.arg("--target=wasm32-wasip1")
@@ -21,44 +19,9 @@ fn main() -> io::Result<()> {
 		.arg("--release")
 		.arg("--target-dir=target")
 		.arg("--artifact-dir")
-		.arg(&out_dir);
-	let status = cargo.status()?;
+		.arg(&out_dir)
+		.status()?;
 	assert!(status.success());
 
 	Ok(())
-}
-
-pub fn cargo() -> Command {
-	sanitize("cargo")
-}
-
-fn sanitize(cmd: &str) -> Command {
-	let cmd = {
-		let exe = format!("{cmd}{}", env::consts::EXE_SUFFIX);
-		// On windows, the userspace toolchain ends up in front of the rustup proxy in $PATH.
-		// To reach the rustup proxy nonetheless, we explicitly query $CARGO_HOME.
-		let mut cargo_home = PathBuf::from(env::var_os("CARGO_HOME").unwrap());
-		cargo_home.push("bin");
-		cargo_home.push(&exe);
-		if cargo_home.exists() {
-			cargo_home
-		} else {
-			PathBuf::from(exe)
-		}
-	};
-
-	let mut cmd = Command::new(cmd);
-
-	// Remove rust-toolchain-specific environment variables from kernel cargo
-	cmd.env_remove("LD_LIBRARY_PATH");
-	env::vars()
-		.filter(|(key, _value)| {
-			key.starts_with("CARGO") && !key.starts_with("CARGO_HOME")
-				|| key.starts_with("RUST") && !key.starts_with("RUSTUP_HOME")
-		})
-		.for_each(|(key, _value)| {
-			cmd.env_remove(&key);
-		});
-
-	cmd
 }
