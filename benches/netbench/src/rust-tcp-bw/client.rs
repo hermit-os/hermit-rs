a80b46f23160f8e4aa7a554fe105c0c0dcf6a39a
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2025-05-07 17:35:41 +0200

    fix(netbench): inline `n_rounds`, `n_bytes`

--- benches/netbench/src/rust-tcp-bw/client.rs
+++ benches/netbench/src/rust-tcp-bw/client.rs
@@ -12,17 +12,15 @@ fn main() {
 	let args = Config::parse();
 
 	println!("Connecting to the server {}...", args.address);
-	let n_rounds = args.n_rounds;
-	let n_bytes = args.n_bytes;
 
 	if let Ok(mut stream) = connection::client_connect(args.address_and_port()) {
 		connection::setup(&args, &stream);
 		println!("Connection established! Ready to send...");
 
 		// Create a buffer of 0s, size n_bytes, to be sent over multiple times
-		let buf = vec![0; n_bytes];
+		let buf = vec![0; args.n_bytes];
 
-		for _i in 0..n_rounds {
+		for _i in 0..args.n_rounds {
 			let mut pos = 0;
 
 			while pos < buf.len() {
--- benches/netbench/src/rust-tcp-bw/server.rs
+++ benches/netbench/src/rust-tcp-bw/server.rs
@@ -11,10 +11,9 @@ use rust_tcp_io_perf::{connection, print_utils};
 
 fn main() {
 	let args = Config::parse();
-	let n_bytes = args.n_bytes;
 	let tot_bytes = args.n_rounds * args.n_bytes;
 
-	let mut buf = vec![0; n_bytes];
+	let mut buf = vec![0; args.n_bytes];
 
 	let mut stream = connection::server_listen_and_get_first_connection(&args.port.to_string());
 	connection::setup(&args, &stream);
