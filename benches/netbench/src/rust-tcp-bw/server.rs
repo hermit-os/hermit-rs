a8fa976fd9f8cefe4ba89b0a2f981623155200a9
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2026-02-11 12:14:30 +0100

    benches: netbench-tcp-bw: Proper average throughput calculation

--- benches/netbench/src/rust-tcp-bw/server.rs
+++ benches/netbench/src/rust-tcp-bw/server.rs
@@ -10,14 +10,13 @@ use rust_tcp_io_perf::connection;
 
 fn main() {
 	let args = Config::parse();
-	let tot_bytes = args.n_rounds * args.n_bytes;
 
 	let mut buf = vec![0; args.n_bytes];
+	let mut durations = Vec::with_capacity(args.n_rounds);
 
 	let mut stream = connection::server_listen_and_get_first_connection(&args.port.to_string());
 	connection::setup(&args, &stream);
 
-	let start = Instant::now();
 	for i in 0..args.n_rounds {
 		print!("round {i}: ");
 		let round_start = Instant::now();
@@ -25,15 +24,13 @@ fn main() {
 		let round_end = Instant::now();
 		let duration = round_end.duration_since(round_start);
 		let mbits = buf.len() as f64 * 8.0f64 / (1024.0f64 * 1024.0f64 * duration.as_secs_f64());
-		println!("{mbits} Mbit/s");
+		durations.push(mbits);
 	}
-	let end = Instant::now();
-	let duration = end.duration_since(start);
 
 	log_benchmark_data(
 		"TCP server",
 		"Mbit/s",
-		(tot_bytes as f64 * 8.0f64) / (1024.0f64 * 1024.0f64 * duration.as_secs_f64()),
+		durations.iter().sum::<f64>() / durations.len() as f64,
 	);
 
 	connection::close_connection(&stream);
