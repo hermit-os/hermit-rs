8fc368bc7ac0b82959f65d76930a493e80ec6847
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2026-02-11 21:23:31 +0100

    add basic support of the system call 'fork'
    In the long term, Hermit will also be usable as a monolithic OS.
    To prepare for this, an example of calling the fork system call
    is being integrated. In the case of a unikernel, the fork call
    will return an error (ENOSYS).

--- hermit/src/syscall/x86_64.rs
+++ hermit/src/syscall/x86_64.rs
@@ -56,9 +56,10 @@ macro_rules! syscall {
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall0(arg0: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			lateout("rcx") _,
 			lateout("r11") _,
@@ -71,9 +72,10 @@ pub(crate) fn syscall0(arg0: u64) -> u64 {
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall1(arg0: u64, arg1: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			in("rdi") arg1,
 			lateout("rcx") _,
@@ -87,9 +89,10 @@ pub(crate) fn syscall1(arg0: u64, arg1: u64) -> u64 {
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall2(arg0: u64, arg1: u64, arg2: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			in("rdi") arg1,
 			in("rsi") arg2,
@@ -104,9 +107,10 @@ pub(crate) fn syscall2(arg0: u64, arg1: u64, arg2: u64) -> u64 {
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall3(arg0: u64, arg1: u64, arg2: u64, arg3: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			in("rdi") arg1,
 			in("rsi") arg2,
@@ -122,9 +126,10 @@ pub(crate) fn syscall3(arg0: u64, arg1: u64, arg2: u64, arg3: u64) -> u64 {
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall4(arg0: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			in("rdi") arg1,
 			in("rsi") arg2,
@@ -141,10 +146,11 @@ pub(crate) fn syscall4(arg0: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64) ->
 #[allow(dead_code)]
 #[inline]
 pub(crate) fn syscall5(arg0: u64, arg1: u64, arg2: u64, arg3: u64, arg4: u64, arg5: u64) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
-			inlateout("rax") arg0 => ret,
+		asm!(
+			"syscall",
+			inlateout("rax") arg0 =>ret,
 			in("rdi") arg1,
 			in("rsi") arg2,
 			in("rdx") arg3,
@@ -169,9 +175,10 @@ pub(crate) fn syscall6(
 	arg5: u64,
 	arg6: u64,
 ) -> u64 {
-	let ret: u64;
+	let mut ret: u64;
 	unsafe {
-		asm!("syscall",
+		asm!(
+			"syscall",
 			inlateout("rax") arg0 => ret,
 			in("rdi") arg1,
 			in("rsi") arg2,
