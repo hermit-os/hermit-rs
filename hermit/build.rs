4c48147e4d958dedbb2b194743f677645d1e26aa
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-02-11 16:16:33 +0100

    fix(build.rs): track missing env vars

--- hermit/build.rs
+++ hermit/build.rs
@@ -185,11 +185,17 @@ impl KernelSrc {
 		);
 
 		println!("cargo:rerun-if-env-changed=HERMIT_CAREFUL");
+		println!("cargo:rerun-if-env-changed=HERMIT_DNS1");
+		println!("cargo:rerun-if-env-changed=HERMIT_DNS2");
+		println!("cargo:rerun-if-env-changed=HERMIT_GATEWAY");
+		println!("cargo:rerun-if-env-changed=HERMIT_IP");
 		println!("cargo:rerun-if-env-changed=HERMIT_LOG_LEVEL_FILTER");
 		println!("cargo:rerun-if-env-changed=HERMIT_MANIFEST_DIR");
+		println!("cargo:rerun-if-env-changed=HERMIT_MASK");
 		println!("cargo:rerun-if-env-changed=HERMIT_MRG_RXBUF_SIZE");
 		println!("cargo:rerun-if-env-changed=HERMIT_MTU");
 		println!("cargo:rerun-if-env-changed=NO_COLOR");
+		println!("cargo:rerun-if-env-changed=UHYVE_MOUNT");
 	}
 
 	fn rerun_if_changed_cargo(&self, cargo_toml: &Path) {
